e05b7b6431a677c93e863d97f60ced41cd99ea0d
Andrew Friend <andrewfriend53@gmail.com>
2021-11-03 11:47:45 +0000

    moved video class to own file

---
+++ prototype/classes/Video.js
@@ -0,0 +1,61 @@
+import { spawnSync } from "child_process";
+import { path as ffprobePath } from 'ffprobe-static';
+
+class Video{
+    constructor(name,videoDirectory){
+        let path = videoDirectory+"/"+name;
+        if(!existsSync(path)){
+            throw new VideoNotFoundError();
+        }
+        /* Copyright (c) 2016, Eugene Ware 
+          uses code adapted from : https://github.com/eugeneware/ffprobe/blob/master/index.js */
+        const params = ['-v quiet','-show_streams', '-print_format', 'json', path];
+        let result = spawnSync(ffprobePath,params)
+        let stdout = JSON.parse(result.stdout.toString())
+        let streams = stdout.streams;
+        let videoData = stdout.streams[0];
+        this._duration = videoData.duration
+        this._codec = codec_name
+        this._path = path
+
+        //now get video metadata
+         
+    }
+
+    /**
+     * duration in seconds
+     * @returns {number}
+     */
+    get duration(){
+        return this._duration
+    }
+    
+    /**
+     * codec of video
+     * @returns {string}
+     */
+    get codec(){
+        return this._codec
+    }
+
+    get path(){
+        return this._path
+    }
+    
+
+}
+
+class VideoNotFoundError extends Error{
+    constructor(){
+        super("Video Not Found");
+    }
+}
+
+class ffprobeError extends Error{
+    constructor(stderrBuffer){
+        super("ffprobe error")
+        this.stderr = stderrBuffer.toString()
+    }
+}
+
+export {Video,VideoNotFoundError,ffprobeError};
