cf0a711a853e617630fc68877073b12836ba5add
Andrew Friend <andrewfriend53@gmail.com>
2021-11-26 22:25:51 +0000

    fixes spelling error in domain

--- prototype/server.js
+++ prototype/server.js
@@ -36,7 +36,7 @@ const videos = screens.flatMap(screen => screen.videos);
 const addresses = {
     v4External: await publicIp.v4(),
     v4Internal: internalIpV4Sync(),
-    domain: "project.andrewfriend.xyx",
+    domain: "project.andrewfriend.xyz",
     ssl: false, 
     port:port 
     //add  later ipv6, will require proper error handling
