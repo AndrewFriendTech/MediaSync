353c7c52e4e3609538075a2cf1b70793e2e5685b
Andrew Friend <andrewfriend53@gmail.com>
2022-03-02 22:55:10 +0000

    removed unesscary function

--- main/designer/src/index.js
+++ main/designer/src/index.js
@@ -13,26 +13,6 @@ function onSave(){
     
 }
 
-function previewFile() {
-    const fileElement = document.querySelector('input[type=file]')
-    if(!fileElement){
-        return  
-    } 
-    const file = fileElement.files[0];
-    const reader = new FileReader();
-  
-    reader.addEventListener("load", function () {
-      // convert image file to base64 string
-        console.log(reader.result);
-    }, false);
-  
-    if (file) {
-      reader.readAsDataURL(file);
-    }
-}
-
-
-
 
 window.onload = () => {
   document.getElementById("video-upload-button")
